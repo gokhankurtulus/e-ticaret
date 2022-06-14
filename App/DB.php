e8e8f0250d024146e83da4be54d6ee83842ca49f
Gökhan Kurtuluş <gokhankurtulus@icloud.com>
2022-06-14 06:55:06 +0300

    added db

---
+++ App/DB.php
@@ -0,0 +1,13 @@
+<?php
+
+class DB {
+    public function __construct()
+    {
+        $serverName = "localhost";
+        $dbName = "xxx";
+        $dbUsername = "xxx";
+        $dbPassword = "xxx";
+        $db = new PDO("mysql:host=$serverName;dbname=$dbName;charset=utf8", $dbUsername, $dbPassword);
+        $this->_db = $db;
+    }
+}
