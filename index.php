7dd14b6670935a5d2daec3c2af2027be2b7ce507
Gökhan Kurtuluş <gokhankurtulus@icloud.com>
2022-07-08 15:32:41 +0300

    update index.php

--- index.php
+++ index.php
@@ -2,84 +2,43 @@
 error_reporting(1);
 session_start();
 
-$requestUrl = $_SERVER['REQUEST_URI'];
-try {
-    $parsed_url = explode('/', $requestUrl);
-    $params['page'] = $parsed_url[2];
-    $params['value'] = $parsed_url[3];
-    $params['sort'] = $parsed_url[4];
-    $params['val'] = $parsed_url[5];
-} catch (Exception $exception) {
-}
+if (isset($_REQUEST['logout']))
+    unset($_SESSION['login']);
 
-spl_autoload_register(function ($class) {
-    if (is_file($class.'.php')) {
-        require_once($class.'.php');
-    }
-});
+$providerPath = 'App/Providers/Provider.php';
 
-use App\Models\{User};
-use App\Providers\{Provider, Role, Route, Assets, Functions, Error};
-use App\Controllers\{UserController, User\LoginController};
+if (is_file($providerPath))
+    require_once $providerPath;
+
+use App\Providers\{Provider};
+use function App\Providers\view;
+use App\Controllers\{User\UserController};
 
 Provider::boot();
 
-//    $user = UserController::get(['username' => 'gokhankurtulus']);
-//    echo $user->getName();
-
-
-//    if (isset($_REQUEST['username'])) {
-//        var_dump($_REQUEST);
-//        $user = UserController::get($_REQUEST);
-//        echo $user->getName();
-//    }
-
-//    UserController::delete(['id'=>37]);
-//    $user = UserController::update(identifier: ['username' => 'gokhan3kurtulus', 'name' => 'Gökhan'], where: ['id' => 1, 'username' => 'gokhan2kurtulus']);
-//    var_dump($user);
-//    $user = UserController::get(['username' => 'gokhankurtulus']);
-//    if ($user)
-//        echo $user->getName();
-//    $users = UserController::search('name', 'gok');
-//    echo '<pre>';
-//    var_dump($users);
-//    echo '</pre>';
-//    $atts = ['name'=>'Gökhan','surname'=>'kurtulus','phone'=>1234];
-//    $sonuc =  UserController::create($atts);
-//    var_dump($sonuc);
-//echo Functions::isStringLenghtBetween('heyasdasd',6,20);
-if (isset($_REQUEST['type'])) {
-    if ($_REQUEST['type'] === 'LoginviaUsername') {
-        $login = LoginController::authenticate($_REQUEST);
+if (isset($_SESSION['login'])) {
+    $loginedUser = UserController::get(['id' => $_SESSION['login']]);
+    if (!is_a($loginedUser, Error::class)) {
+        $GLOBALS['loginedUser'] =
+            [
+                'id' => $loginedUser->getID(),
+                'username' => $loginedUser->getUsername(),
+                'name' => $loginedUser->getName(),
+                'verified' => $loginedUser->getVerified(),
+                'role' => $loginedUser->getRole()
+            ];
     }
-    if ($_REQUEST['type'] === 'LoginviaMail') {
-        $login = LoginController::authenticate($_REQUEST);
-    }
-    if ($_REQUEST['type'] === 'LoginviaPhone') {
-        $login = LoginController::authenticate($_REQUEST);
-    }
-    if (!is_a($login, Error::class))
-        var_dump($login);
-    else
-        echo $login->message();
 }
-?>
-<form method="post">
-    <input type="hidden" name="type" value="LoginviaUsername">
-    <input type="text" name="username" value="">
-    <input type="text" name="password" value="">
-    <input type="submit" name="login">
-</form>
 
-<form method="post">
-    <input type="hidden" name="type" value="LoginviaMail">
-    <input type="text" name="mail" value="">
-    <input type="text" name="password" value="">
-    <input type="submit" name="login">
-</form>
-<form method="post">
-    <input type="hidden" name="type" value="LoginviaPhone">
-    <input type="text" name="phone" value="">
-    <input type="text" name="password" value="">
-    <input type="submit" name="login">
-</form>
+
+if ($GLOBALS['route']->getPage() == 'login')
+    view("Page/login.phtml");
+else if ($GLOBALS['route']->getPage() == 'register')
+    view("Page/register.phtml");
+else if ($GLOBALS['route']->getPage() == 'logout') {
+    view("Page/logout.phtml");
+} else if ($GLOBALS['route']->getPage() == 'panel')
+    view("Panel/layout/panel.phtml");
+else
+    view("Page/layout/page.phtml");
+?>
